affe6856b00aa83bf84207e7a2d95ef8a5400370
Adrian Khor <adriank96@outlook.com>
2021-11-19 22:13:42 +0900

    practice 2

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"math"
+	"math/cmplx"
 	"math/rand"
 	"time"
 	//"rsc.io/quote"
@@ -13,7 +15,82 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
 func main() {
+	fmt.Println("The time is ", time.Now(), ". OK!")
+	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println("The sqrt for 64 is", math.Sqrt(64))
+	fmt.Println(math.Pi)
+	fmt.Println(swap("Hello", "World"))
+	fmt.Println("The length of the string 'foo' is", string_length("foo"))
+
+	// var i int
+	// var foo bool
+	var i, foo = 42, true
+	// k := 3 is a shorthand for var k = 3
+	k := 3
+	fmt.Println("Value of i is", i)
+	fmt.Println("Value of foo is", foo)
+	fmt.Println("Value of k is", k)
+
+	// for loop 1
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
+	// // for loop 2
+	// sum2 := 0
+	// for i := 0; i < 10; {
+	// 	sum2 += i
+	// }
+	// fmt.Println(sum2)
+
+	// for loop 3
+	sum3 := 0
+	for sum3 < 10 {
+		sum3++
+	}
+	fmt.Println(sum3)
+
+	// for loop 4
+	sum4 := 0
+	// essentially is while loop
+	for {
+		if sum4 >= 20 {
+			break
+		}
+		fmt.Println(sum4)
+		sum4++
+	}
+}
+
+func pow(x, n, lim float64) float64 {
+	// if with short statement
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func swap(x string, y string) (string, string) {
+	return y, x
+}
+
+// Gets the length of a given string
+// Named return demo
+func string_length(s string) (length int) {
+	length = len(s)
+	return
+}
+
+func main_1() {
 	// Set properties of the predefined Logger, including
 	// the log entry prefix and a flag to disable printing
 	// the time, source file, and line number.
@@ -37,7 +114,6 @@ func main() {
 	}
 
 	fmt.Println(result)
-
 }
 
 func Add(n1 int, n2 int) int {
